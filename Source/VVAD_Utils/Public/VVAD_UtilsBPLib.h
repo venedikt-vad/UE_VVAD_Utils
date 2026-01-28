7de856d5bed2c6069059cd3e722694e79c90298d
VVAD <venya_kozlov_00@mail.ru>
2026-01-29 01:23:06 +0600

    [*] Updating to UE5.7. Removed CompactNodeTitles to keep them low priority in search

--- Source/VVAD_Utils/Public/VVAD_UtilsBPLib.h
+++ Source/VVAD_Utils/Public/VVAD_UtilsBPLib.h
@@ -69,14 +69,14 @@ public:
     UFUNCTION(BlueprintPure, Category = "Math|Float", meta = ( DisplayName = "(float) *-1", KeyWords = ""))
     static float NegateFloat(const float In);
     
-    UFUNCTION(BlueprintPure, Category = "Math|Float", meta = (CompactNodeTitle = "1 - X", DisplayName = "(float) 1-x", KeyWords = ""))
+    UFUNCTION(BlueprintPure, Category = "Math|Float", meta = ( DisplayName = "(float) 1-x", KeyWords = ""))
     static float OneMinusX(const float X);
 
     //Returns 1 if true, -1 if false
     UFUNCTION(BlueprintPure, Category = "Math|Float", meta = (CompactNodeTitle = "? 1 : -1", DisplayName = "(float) True+False-", KeyWords = "True+False-"))
     static float TruePositiveFalseNegative(const bool In);
 
-    UFUNCTION(BlueprintPure, Category = "Math|Float", meta = (CompactNodeTitle = "-1", DisplayName = "(float) -1", KeyWords = ""))
+    UFUNCTION(BlueprintPure, Category = "Math|Float", meta = (DisplayName = "(float) -1", KeyWords = ""))
     static float FloatMinusOne(const float In);
 
     UFUNCTION(BlueprintPure, Category = "Math|Float", meta = (CompactNodeTitle = "+1", DisplayName = "(float) +1", KeyWords = ""))
@@ -86,7 +86,7 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Math|Float", meta = (CompactNodeTitle = "+=", DisplayName = "(float) +=", KeyWords = "float add"))
     static void AddAssignF(UPARAM(ref) float& Variable, const float add);
 
-    UFUNCTION(BlueprintCallable, Category = "Math|Float", meta = (CompactNodeTitle = "-=", DisplayName = "(float) -= ", KeyWords = "float subtract"))
+    UFUNCTION(BlueprintCallable, Category = "Math|Float", meta = (DisplayName = "(float) -= ", KeyWords = "float subtract"))
     static void SubtAssignF(UPARAM(ref) float& Variable, const float subtract);
 
     UFUNCTION(BlueprintCallable, Category = "Math|Float", meta = ( DisplayName = "(float) /="))
@@ -103,19 +103,19 @@ public:
     UFUNCTION(BlueprintPure, Category = "Math|Integer", meta = (DisplayName = "(Integer) *-1"))
     static int32 NegateInt(const int32 In);
     
-    UFUNCTION(BlueprintPure, Category = "Math|Integer", meta = (CompactNodeTitle = "-1", DisplayName = "(Integer) -1", KeyWords = ""))
+    UFUNCTION(BlueprintPure, Category = "Math|Integer", meta = (DisplayName = "(Integer) -1", KeyWords = ""))
     static int32 IntMinusOne(const int32 In);
 
-    UFUNCTION(BlueprintPure, Category = "Math|Integer", meta = (CompactNodeTitle = "+1", DisplayName = "(Integer) +1", KeyWords = ""))
+    UFUNCTION(BlueprintPure, Category = "Math|Integer", meta = (DisplayName = "(Integer) +1", KeyWords = ""))
     static int32 IntPlusOne(const int32 In);
 
-    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = (CompactNodeTitle = "+=", DisplayName = "(Integer) +=", KeyWords = "Integer add"))
+    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = ( DisplayName = "(Integer) +=", KeyWords = "Integer add"))
     static void AddAssignInt(UPARAM(ref) int32& Variable, const int32 add);
 
-    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = (CompactNodeTitle = "-=", DisplayName = "(Integer) -= ", KeyWords = "Integer subtract"))
+    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = ( DisplayName = "(Integer) -= ", KeyWords = "Integer subtract"))
     static void SubtAssignInt(UPARAM(ref) int32& Variable, const int32 subtract);
 
-    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = (CompactNodeTitle = "/=", DisplayName = "(Integer) /=", KeyWords = "Integer divide"))
+    UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = ( DisplayName = "(Integer) /=", KeyWords = "Integer divide"))
     static void DivideAssignInt(UPARAM(ref) int32& Variable, const int32 divider);
 
     UFUNCTION(BlueprintCallable, Category = "Math|Integer", meta = (CompactNodeTitle = "*=", DisplayName = "(Integer) *=", KeyWords = "Integer multiply"))
